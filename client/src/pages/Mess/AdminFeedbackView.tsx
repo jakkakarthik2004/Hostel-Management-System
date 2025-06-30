d41f9ac5649ffc3a746ee1e891d1bacf3917f466
jakkakarthik04 <jsunnybabu@gmail.com>
2025-06-30 16:25:55 +0530

    small changes

--- client/src/pages/Mess/AdminFeedbackView.tsx
+++ client/src/pages/Mess/AdminFeedbackView.tsx
@@ -225,7 +225,6 @@ export default function AdminFeedbackList() {
             </CardContent>
           </Card>
 
-          {/* Pie Chart - Rating Distribution */}
           <Card>
             <CardHeader>
               <CardTitle>Meal Rating Distribution</CardTitle>
